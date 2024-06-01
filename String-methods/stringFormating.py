56662ed9bdd42a33c45c1c0eeeb7284bf724e76f
Ahsan-Ali <149705869+Ahsanali18@users.noreply.github.com>
2024-06-01 21:15:35 +0500

    Delete String-methods directory

--- String-methods/stringFormating.py
+++
@@ -1,9 +0,0 @@
-firstName="John"
-lastName="Smith"
-message=firstName+'['+lastName+'] is a coder'
-print(message)
-
-# Same message printing by format String
-
-msg=f'{firstName}[{lastName}]is a coder'
-print(msg)
