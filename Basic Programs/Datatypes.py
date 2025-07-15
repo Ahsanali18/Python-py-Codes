3261a18590688d8b27bd8df9eb201e035ad3ce2b
Ahsan-Ali <149705869+Ahsanali18@users.noreply.github.com>
2025-07-15 11:48:36 +0500

    Add files via upload

---
+++ Basic Programs/Datatypes.py
@@ -0,0 +1,27 @@
+number = 5
+print(type(number))
+
+average = 10.5
+print(type(average))
+
+complex_number = 2+3j
+print(type(complex_number))
+
+complex_num = complex(2,4)
+print(type(complex_num))
+
+is_programmer = True
+print(type(is_programmer))
+
+is_programmer = int(is_programmer)
+print(is_programmer)
+
+is_doctor = False
+print(type(is_doctor))
+is_doctor = int(is_doctor)
+print(type(is_doctor))
+
+print(range(10))
+print(type(range(10)))
+
+print(list(range(10)))
---
+++ Basic Programs/Variables2.py
@@ -0,0 +1,23 @@
+num = 5
+print(id(num))  #gives the <address> of num variable in memory
+
+name = "ahsan"
+print(id(name))
+
+a = 10
+b = a
+print(a,b)
+print(id(a))
+print(id(b))
+
+k = 10
+print(id(k))
+
+a = 9
+print(id(a))
+
+print(b)   #b is still pointing to 10
+print(id(b))
+
+# PI = 3.14
+# print(PI)
